da54b440b2f44f445b76cd4d4e529dbb2cc05cb8
刘程旭 <>
2021-03-05 18:57:17 +0800

    用工具提交一下

---
+++ 2.py
@@ -0,0 +1,20 @@
+"action": [
+            {
+                "name": "confirm",
+                "value": "业务透传数据",
+                "text": "提交",
+                "color": "0F82F0",
+                "confirm":{
+                	"title":"确定提交吗",
+                	"content":"",
+                	"ok":"确定",
+                	"cancel":"取消"
+                }
+            },
+            {
+                "name": "cancel",
+                "value": "业务透传数据",
+                "text": "取消",
+                "color": "979CA4"
+            }
+        ]
